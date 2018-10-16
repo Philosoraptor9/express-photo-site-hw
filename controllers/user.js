e28a0d9a74749df70c8e235816586949e43abd77
Philip Hoover <philhoov90@gmail.com>
2018-10-15 21:47:46 -0600

    test routes working

--- controllers/user.js
+++ controllers/user.js
@@ -2,4 +2,25 @@ const express = require('express');
 const User = require('../models/user');
 const router = express.Router();
 
+// Index route - shows all usernames
+router.get('/', (req, res) => {
+    res.render('users/index.ejs');
+});
+
+// New route - add a new username and password
+router.get('/new', (req, res) => {
+    res.render('users/new.ejs');
+});
+
+// Show route - shows user's username, edit and delete buttons and all photos (later)
+router.get('/:id', (req, res) =>{
+    res.render('users/show.ejs');
+});
+
+// Update and delete - edit button should take you to 'edit' form, delete button should delete user
+router.get('/:id/edit', (req, res) =>{
+    res.render('users/edit.ejs');
+});
+
+
 module.exports = router;
