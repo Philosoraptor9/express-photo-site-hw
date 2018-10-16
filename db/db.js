a1aa0c1475a32e9540befda107dd8638643e8512
Philip Hoover <philhoov90@gmail.com>
2018-10-15 20:52:13 -0600

    server, db set up

---
+++ db/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const connectionString = "mongodb://localhost/photo-blog";
+
+mongoose.connect(connectionString, { useNewUrlParser: true});
+
+mongoose.connection.on('connected', ()=>{
+    console.log(`mongoose is connected to ${connectionString}`);
+})
+
+mongoose.connection.on('disconnected', ()=>{
+    console.log("mongoose is disconnected");
+})
+
+mongoose.connection.on('error', (err)=>{
+    console.log("mongoose error is");
+    console.log(err)
+})
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const morgan = require('morgan');
+
+require('./db/db');
+
+app.use(morgan('short'));
+app.use(methodOverride('_method'));
+app.use(bodyParser({urlencoded: true, extended: false}));
+
+const userController = require('./controllers/user');
+
+// app.use('/users', userController);
+
+app.get('/', (req, res)=>{
+    res.render('users/index.ejs');
+})
+
+
+const port = 3000;
+
+app.listen(port, ()=>{
+    console.log(`Up and running on ${port}`);
+})
