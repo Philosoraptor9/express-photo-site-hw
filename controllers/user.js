990bba47f8fbe260df9bf62578f2a1a72687866d
Philip Hoover <philhoov90@gmail.com>
2018-10-15 22:52:56 -0600

    routes are working... wtf is wrong w/ my database??

--- controllers/user.js
+++ controllers/user.js
@@ -4,7 +4,10 @@ const router = express.Router();
 
 // Index route - shows all usernames
 router.get('/', (req, res) => {
-    res.render('users/index.ejs');
+    User.find({}, (err, foundUsers) => {
+    console.log(foundUsers);
+    res.render('users/index.ejs', {users: foundUsers});
+    })
 });
 
 // New route - add a new username and password
@@ -14,13 +17,42 @@ router.get('/new', (req, res) => {
 
 // Show route - shows user's username, edit and delete buttons and all photos (later)
 router.get('/:id', (req, res) =>{
-    res.render('users/show.ejs');
+    User.findById(req.params.id, (err, foundUser) => {
+        console.log(foundUser);
+        res.render('users/show.ejs', {user: foundUser});
+    })
 });
 
 // Update and delete - edit button should take you to 'edit' form, delete button should delete user
 router.get('/:id/edit', (req, res) =>{
-    res.render('users/edit.ejs');
+    User.findById(req.params.id, (err, foundUser) => {
+        console.log(foundUser);
+        res.render('users/edit.ejs');
+    })
 });
 
+router.post('/', (req, res) => {
+    console.log(`req.body is ${JSON.stringify(req.body)}`);
+    User.create(req.body, (err, newUser) =>{
+        console.log(newUser);
+        if (err) {
+            console.log(err)
+        } else {
+            res.redirect('/users')
+        }
+    })
+});
+
+router.delete('/:id', (req, res)=>{
+    User.findByIdAndDelete(req.params.id, (err, result)=>{
+        res.redirect('/users')
+    })
+});
+
+router.put('/:id', (req, res)=>{
+    User.findByIdAndUpdate(req.params.id, req.body, (err, newUser)=>{
+        res.redirect(`/users/${req.params.id}`)
+    })
+});
 
 module.exports = router;
